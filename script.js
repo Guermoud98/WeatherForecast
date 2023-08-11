450d6070b2441e037c9992ea5d8dfe0958ea2e71
Gur <guermoud.@gmail.com>
2023-08-11 13:35:12 +0100

    Added a JSON file containing a list of major cities in European countries with their longitude and latitude

---
+++ script.js
@@ -0,0 +1,10 @@
+/*fetch('http://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&product=civillight&output=json')
+.then(response => response.json())
+.then(data => {
+    console.log(data); // Process the parsed JSON data
+  })
+.catch(error => {
+    console.error('Fetch error:', error);
+  });
+  */
+ 
