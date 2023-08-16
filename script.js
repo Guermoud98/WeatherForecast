7ec37ed30ff18da977a42e3770f53cd4203df245
Gur <guermoud.@gmail.com>
2023-08-16 18:38:48 +0100

    Displaying results received from the API on the HTML page

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ import {
 
 //console.log(cities);
 var selectElement = document.getElementById("mySelect");
+var parent = document.getElementById("container");
+var childDivs = document.querySelectorAll(".child");
+var date = document.querySelectorAll(".date");
+var img = document.querySelectorAll(".img");
+var weather = document.querySelectorAll(".weather");
+var max = document.querySelectorAll(".max");
+var min = document.querySelectorAll(".min");
 
 function retrievingCities() {
     var j = 0;
@@ -29,6 +36,16 @@ function citiesLonLat(longitude, latitude) {
         .then(response => response.json())
         .then(data => {
             console.log(data); // Process the parsed JSON data
+            for (var i = 0; i < data.dataseries.length; i++) {
+                var dataseries = data.dataseries[i];
+                weather[i].textContent = dataseries.weather;
+                max[i].textContent += dataseries.temp2m.max;
+                min[i].textContent += dataseries.temp2m.min;
+                date[i].textContent = dataseries.date;
+                
+
+            }
+
         })
         .catch(error => {
             console.error('Fetch error:', error);
@@ -39,13 +56,14 @@ function citiesLonLat(longitude, latitude) {
 //citiesLonLat() ;
 var citiesData = retrievingCities();
 console.log(citiesData);
+
+
 selectElement.addEventListener('change', function () {
     var selectedValue = selectElement.value;
     console.log('Selected option value:', selectedValue);
     for (var i = 0; i < citiesData.length; i++) {
         var cityGroup = citiesData[i];
         for (var j = 0; j < cityGroup.length; j++) {
-
             if (selectedValue === cityGroup[j].name) {
                 citiesLonLat(cityGroup[j].longitude, cityGroup[j].latitude);
                 break;
@@ -55,6 +73,3 @@ selectElement.addEventListener('change', function () {
     }
 
 })
-
-
-
